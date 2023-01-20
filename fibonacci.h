76c73d1a0ca89ac3fcced65dd87a088ad2f66ed7
Venera777 <122600316+Venera777@users.noreply.github.com>
2023-01-20 21:45:38 +0400

    Add files via upload

---
+++ fibonacci.h
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+void multiply(int F[2][2], int M[2][2]);
+void power(int F[2][2], int n);
+int fib(int n);
---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+#include "fibonacci.h"
+
+int main() {
+    int a = 20;
+
+    cout << fib(a);
+
+    return 0;
+}
